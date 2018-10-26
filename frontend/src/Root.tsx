1c82cb6fad785f499396d2e0eca14b76da6dea3d
martin <martin@tmon.co.kr>
2018-10-26 18:47:17 +0900

    Delete page's App file

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import App from './pages/App';
+import { Route, Switch } from 'react-router-dom';
+import Navigator from "./components/Navigator";
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+// import { Router, Router } from 'react-router'
+import { routerMiddleware, routerReducer } from 'react-router-redux'
+
+import {
+    Home,
+    RegisterBoard,
+    Signin,
+    Signup,
+    ViewBoard
+} from "./pages";
 
 const Root = () => {
     return (
         <BrowserRouter>
-            <App/>
+            <div>
+                <Navigator/>
+                <Switch>
+                    <Route exact={true} path="/" component={Home}/>
+                    <Route path="/signin/:param" component={Signin}/>
+                    <Route path="/signin" component={Signin}/>
+                    <Route exact={true} path="/signup" component={Signup}/>
+                    <Route path="/register" component={RegisterBoard}/>
+                    <Route path="/:index" component={ViewBoard}/>
+                </Switch>
+            </div>
         </BrowserRouter>
     )
 };
